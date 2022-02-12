3ef8cf1430727209271c9cc95b5574ce3e942f15
Chetan Gaikwad <chetan@ridecell.com>
2022-02-13 00:13:36 +0530

    Updated libs

--- app/src/main/java/mobileutility/mybledemo/MainActivity.java
+++ app/src/main/java/mobileutility/mybledemo/MainActivity.java
@@ -22,13 +22,14 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
